2fa8cd3a4780ab9b7200323ab1d16df7a8d4605c
Gallinator <lcgallinator4@gmail.com>
2025-05-22 21:28:34 +0200

    training: initial commit
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

---
+++ training.py
@@ -0,0 +1,24 @@
+from lightning.pytorch.callbacks import RichProgressBar
+from torch.utils.data import DataLoader
+from foam_dataset import FoamDataset
+from models.pipn import Pipn
+import lightning as L
+
+BATCH_SIZE = 13
+N_INTERNAL = 667
+N_BOUNDARY = 168
+
+train_data = FoamDataset('data/train', N_INTERNAL, N_BOUNDARY)
+train_loader = DataLoader(train_data, BATCH_SIZE, True, num_workers=8)
+val_data = FoamDataset('data/eval', N_INTERNAL, N_BOUNDARY)
+val_loader = DataLoader(val_data, BATCH_SIZE, False, num_workers=8, pin_memory=True)
+
+model = Pipn(N_INTERNAL, N_BOUNDARY)
+
+trainer = L.Trainer(max_epochs=-1,
+                    callbacks=[RichProgressBar()],
+                    log_every_n_steps=2,
+                    precision='16',
+                    val_check_interval=2)
+
+trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)

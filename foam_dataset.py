580d8ca030a2bc99c62f44d37063e33cfcf67302
Gallinator <lcgallinator4@gmail.com>
2025-06-08 22:14:45 +0200

    foam_dataset: improve typing
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- foam_dataset.py
+++ foam_dataset.py
@@ -17,15 +17,15 @@ class PdeData:
         return self.data[..., 0:2]
 
     @property
-    def ux(self) -> Tensor:
+    def ux(self) -> Tensor | np.ndarray:
         return self.data[..., 0:1]
 
     @property
-    def uy(self) -> Tensor:
+    def uy(self) -> Tensor | np.ndarray:
         return self.data[..., 1:2]
 
     @property
-    def p(self) -> Tensor:
+    def p(self) -> Tensor | np.ndarray:
         return self.data[..., 2:3]
 
     def numpy(self):
@@ -40,11 +40,11 @@ class FoamData:
         self.f = batch[..., 5:7]
 
     @property
-    def fx(self):
+    def fx(self) -> Tensor | np.ndarray:
         return self.f[..., 5:6]
 
     @property
-    def fy(self):
+    def fy(self) -> Tensor | np.ndarray:
         return self.f[..., 6:7]
 
     @property

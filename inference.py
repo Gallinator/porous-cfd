f299bc158881b93327066bae0731fd5e8e098a67
Gallinator <lcgallinator4@gmail.com>
2025-06-08 16:00:59 +0200

    inference: initial commit
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

---
+++ inference.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+from lightning import Trainer
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+
+from foam_dataset import FoamDataset, PdeData
+from models.pipn import Pipn, FoamData
+from visualization import plot_fields
+
+CHECKPOINT_PATH = 'lightning_logs/version_22/checkpoints/epoch=402-step=806.ckpt'
+
+model = Pipn.load_from_checkpoint(CHECKPOINT_PATH)
+
+train_data = FoamDataset('data/train', 1000, 200, 500)
+val_data = FoamDataset('data/val', 1000, 200, 500, train_data.meta)
+val_loader = DataLoader(val_data, 1, False, num_workers=8, pin_memory=True)
+
+trainer = Trainer(logger=False, enable_checkpointing=False)
+pred = trainer.predict(model, dataloaders=val_loader)[0]
+pred = PdeData(pred).numpy()
+
+tgt = FoamData(val_data[0]).numpy()
+
+points_scaler = train_data.standard_scaler[0:2]
+u_scaler = train_data.standard_scaler[2:4]
+p_scaler = train_data.standard_scaler[4]
+
+raw_points = points_scaler.inverse_transform(tgt.points)
+
+plt.interactive(True)
+plot_fields('Predicted', raw_points, u_scaler.inverse_transform(pred.u[0]),
+            p_scaler.inverse_transform(pred.p[0]), tgt.zones_ids)
+plot_fields('Ground truth', raw_points, u_scaler.inverse_transform(tgt.pde.u),
+            p_scaler.inverse_transform(tgt.pde.p), tgt.zones_ids)
+
+plt.interactive(False)
+
+u_error = u_scaler.inverse_transform(pred.u[0]) - u_scaler.inverse_transform(tgt.pde.u)
+p_error = p_scaler.inverse_transform(pred.p[0]) - p_scaler.inverse_transform(tgt.pde.p)
+plot_fields('Absolute error', raw_points, np.abs(u_error), np.abs(p_error), tgt.zones_ids, plot_streams=False)

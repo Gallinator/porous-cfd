e5d73f2cf5123fa0332c52c849b4e8065d01f964
Gallinator <lcgallinator4@gmail.com>
2025-06-19 11:40:35 +0200

    data_parser: allow parsing of non patch boundaries
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- data_parser.py
+++ data_parser.py
@@ -12,17 +12,18 @@ def parse_boundary(case_path: str, vectors: list[str], scalars: list[str]) -> np
     faces = []
     scalar_values, vector_values = {s: [] for s in scalars}, {v: [] for v in vectors}
     for b in os.listdir(boundaries_path):
-        coords = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/patch_{b}/faceCentres")[None]
+        intermediate_dir = list(os.listdir(f"{boundaries_path}/{b}/surface/{last_step}"))[0]
+        coords = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/{intermediate_dir}/faceCentres")[None]
         coords = make_at_most_2d(coords)
         faces.extend(coords)
 
         for s in scalars:
-            values = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/patch_{b}/scalarField/{s}")[None]
+            values = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/{intermediate_dir}/scalarField/{s}")[None]
             values = make_column(values)
             scalar_values[s].extend(values)
             np.array(scalar_values[s])
         for v in vectors:
-            values = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/patch_{b}/vectorField/{v}")[None]
+            values = FoamFile(f"{boundaries_path}/{b}/surface/{last_step}/{intermediate_dir}/vectorField/{v}")[None]
             values = make_at_most_2d(values)
             vector_values[v].extend(values)
     vector_values = [np.array(vector_values[v]) for v in vectors]

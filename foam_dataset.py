b088f7790a13f57c3922a52ab327c15b5b8ae3fe
Gallinator <lcgallinator4@gmail.com>
2025-06-21 12:37:49 +0200

    foam_dataset: add function to convert StandardScaler to tensor
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- foam_dataset.py
+++ foam_dataset.py
@@ -22,6 +22,9 @@ class StandardScaler:
     def __getitem__(self, item):
         return StandardScaler(self.std[item], self.mean[item])
 
+    def to_torch(self, device=None):
+        return StandardScaler(torch.tensor(self.std, device=device), torch.tensor(self.mean, device=device))
+
 
 class PdeData:
     def __init__(self, data: Tensor | np.ndarray):

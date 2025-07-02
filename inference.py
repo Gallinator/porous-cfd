b8a101e2da448e40c44815260e7fbf89af50a731
Gallinator <lcgallinator4@gmail.com>
2025-07-02 20:52:29 +0200

    inference: fix plots
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- inference.py
+++ inference.py
@@ -18,22 +18,23 @@ trainer = Trainer(logger=False, enable_checkpointing=False)
 pred = trainer.predict(model, dataloaders=val_loader)[0]
 pred = PdeData(pred).numpy()
 
-tgt = FoamData(val_data[0]).numpy()
+tgt = val_data[0]
 
 points_scaler = val_data.standard_scaler[0:2]
 u_scaler = val_data.standard_scaler[2:4]
 p_scaler = val_data.standard_scaler[4]
 
-raw_points = points_scaler.inverse_transform(tgt.points)
+raw_points = points_scaler.inverse_transform(tgt.pos.numpy(force=True))
+ids = tgt.zones_ids.numpy(force=True)[:,1:2]
 
 plt.interactive(True)
-plot_fields('Predicted', raw_points, u_scaler.inverse_transform(pred.u[0]),
-            p_scaler.inverse_transform(pred.p[0]), tgt.zones_ids)
-plot_fields('Ground truth', raw_points, u_scaler.inverse_transform(tgt.pde.u),
-            p_scaler.inverse_transform(tgt.pde.p), tgt.zones_ids)
+plot_fields('Predicted', raw_points, u_scaler.inverse_transform(pred.u),
+            p_scaler.inverse_transform(pred.p), ids)
+plot_fields('Ground truth', raw_points, u_scaler.inverse_transform(tgt.pde.u.numpy(force=True)),
+            p_scaler.inverse_transform(tgt.pde.p.numpy(force=True)), ids)
 
 plt.interactive(False)
 
-u_error = u_scaler.inverse_transform(pred.u[0]) - u_scaler.inverse_transform(tgt.pde.u)
-p_error = p_scaler.inverse_transform(pred.p[0]) - p_scaler.inverse_transform(tgt.pde.p)
-plot_fields('Absolute error', raw_points, np.abs(u_error), np.abs(p_error), tgt.zones_ids, plot_streams=False)
+u_error = u_scaler.inverse_transform(pred.u) - u_scaler.inverse_transform(tgt.pde.u.numpy(force=True))
+p_error = p_scaler.inverse_transform(pred.p) - p_scaler.inverse_transform(tgt.pde.p.numpy(force=True))
+plot_fields('Absolute error', raw_points, np.abs(u_error), np.abs(p_error), ids, plot_streams=False)

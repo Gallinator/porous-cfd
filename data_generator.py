9866d3cace472038f0d8b10fe9bb84483849aa4d
Gallinator <lcgallinator4@gmail.com>
2025-07-13 21:44:01 +0200

    data_generator: fix meta generation indices
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- data_generator.py
+++ data_generator.py
@@ -164,10 +164,10 @@ def generate_meta(data_dir: str):
     elapse_times = []
 
     for case in track(glob.glob(f'{data_dir}/*'), description='Generating metadata'):
-        b_data = parse_boundary(f'{case}', ['U'], ['p'])
+        b_data = parse_boundary(case, ['U'], ['p'])
         b_data = np.concatenate(list(b_data.values()))
-        i_data = parse_internal_mesh(f'{case}', "U", "p")
-        n_porous = np.count_nonzero(b_data[..., -1] > 0) + np.count_nonzero(i_data[..., -1] > 0)
+        i_data = parse_internal_mesh(case, "U", "p")
+        n_porous = np.count_nonzero(b_data[..., -3:-2] > 0) + np.count_nonzero(i_data[..., -3:-2] > 0)
 
         boundary_num_points.append(len(b_data))
         internal_num_points.append(len(i_data))

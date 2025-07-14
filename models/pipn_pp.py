1f5ba456231c7679b39e7dd39b10a3a0395a90dc
Gallinator <lcgallinator4@gmail.com>
2025-07-14 18:37:39 +0200

    PipnPP: dont pass domain data to physical losses
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/pipn_pp.py
+++ models/pipn_pp.py
@@ -104,12 +104,10 @@ class PipnPP(L.LightningModule):
         self.p_scaler = scalers['p']
         self.points_scaler = scalers['Points']
 
-        self.momentum_x_loss = MomentumLoss(0, 1, self.mu, self.d, n_internal,
-                                            self.u_scaler, self.points_scaler, self.p_scaler)
-        self.momentum_y_loss = MomentumLoss(1, 0, self.mu, self.d, n_internal,
-                                            self.u_scaler, self.points_scaler, self.p_scaler)
-        self.continuity_loss = ContinuityLoss(n_internal, self.u_scaler, self.points_scaler)
         self.boundary_loss = BoundaryLoss(n_internal)
+        self.momentum_x_loss = MomentumLoss(0, 1, self.mu, self.u_scaler, self.points_scaler, self.p_scaler)
+        self.momentum_y_loss = MomentumLoss(1, 0, self.mu, self.u_scaler, self.points_scaler, self.p_scaler)
+        self.continuity_loss = ContinuityLoss(self.u_scaler, self.points_scaler)
         self.verbose_predict = False
 
     def forward(self, x: Tensor, pos: Tensor, edge_index: Tensor, batch: Tensor) -> Tensor:

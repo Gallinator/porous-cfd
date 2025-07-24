678cba311286afa5402bba24b5757de301fbdfaa
Gallinator <lcgallinator4@gmail.com>
2025-07-24 20:52:10 +0200

    training: allow setting some parameters with command line arguments
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- training.py
+++ training.py
@@ -1,29 +1,49 @@
+import argparse
+from argparse import ArgumentParser
+
 from lightning.pytorch.callbacks import RichProgressBar
 from torch.utils.data import DataLoader
 from foam_dataset import FoamDataset
 from models.pipn import Pipn
 import lightning as L
 
-BATCH_SIZE = 13
-N_INTERNAL = 1000
-N_BOUNDARY = 200
-N_OBS = 500
 
-train_data = FoamDataset('data/train', N_INTERNAL, N_BOUNDARY, N_OBS)
-train_loader = DataLoader(train_data, BATCH_SIZE, True, num_workers=8)
-val_data = FoamDataset('data/val', N_INTERNAL, N_BOUNDARY, N_OBS, 'data/train')
-val_loader = DataLoader(val_data, BATCH_SIZE, False, num_workers=8, pin_memory=True)
+def build_arg_parser() -> ArgumentParser:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--n-internal', type=int,
+                            help='number of internal points to sample', default=1000)
+    arg_parser.add_argument('--n-boundary', type=int,
+                            help='number of internal points to sample', default=200)
+    arg_parser.add_argument('--n-observations', type=int,
+                            help='number of observation points to sample', default=500)
+    arg_parser.add_argument('--batch-size', type=int, default=13)
+    arg_parser.add_argument('--precision', type=str, default='32-true')
+    return arg_parser
 
-scalers = {'U': train_data.standard_scaler[2:4],
-           'p': train_data.standard_scaler[4],
-           'Points': train_data.standard_scaler[0:2]}
 
-model = Pipn(train_data.domain_dict, scalers)
+if __name__ == '__main__':
+    args = build_arg_parser().parse_args()
 
-trainer = L.Trainer(max_epochs=-1,
-                    callbacks=[RichProgressBar()],
-                    log_every_n_steps=2,
-                    precision='16-mixed',
-                    val_check_interval=2)
+    batch_size = args.batch_size
+    n_internal = args.n_internal
+    n_boundary = args.n_boundary
+    n_obs = args.n_observations
 
-trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    train_data = FoamDataset('data/train', n_internal, n_boundary, n_obs)
+    train_loader = DataLoader(train_data, batch_size, True, num_workers=8)
+    val_data = FoamDataset('data/val', n_internal, n_boundary, n_obs, 'data/train')
+    val_loader = DataLoader(val_data, batch_size, False, num_workers=8, pin_memory=True)
+
+    scalers = {'U': train_data.standard_scaler[2:4],
+               'p': train_data.standard_scaler[4],
+               'Points': train_data.standard_scaler[0:2]}
+
+    model = Pipn(train_data.domain_dict, scalers)
+
+    trainer = L.Trainer(max_epochs=-1,
+                        callbacks=[RichProgressBar()],
+                        log_every_n_steps=2,
+                        precision=args.precision,
+                        val_check_interval=2)
+
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)

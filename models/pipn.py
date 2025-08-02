b96e2280255a1a5eec97c6ca5d16d766c64e763a
Gallinator <lcgallinator4@gmail.com>
2025-08-02 13:45:43 +0200

    pipn: add z losses to loggers
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/pipn.py
+++ models/pipn.py
@@ -66,18 +66,23 @@ class Pipn(L.LightningModule):
                                                'Train loss continuity',
                                                'Train loss momentum x',
                                                'Train loss momentum y',
+                                               'Train loss momentum z',
                                                'Train loss p',
                                                'Train loss ux',
                                                'Train loss uy',
+                                               'Train loss uz',
                                                'Obs loss p',
                                                'Obs loss ux',
                                                'Obs loss uy',
+                                               'Obs loss uz',
                                                'Train p error',
                                                'Train ux error',
-                                               'Train uy error')
+                                               'Train uy error',
+                                               'Train uz error')
         self.val_loss_logger = LossLogger(self, 'Val error p',
                                           'Val error ux',
-                                          'Val error uy')
+                                          'Val error uy',
+                                          'Val error uz')
 
         self.u_scaler = scalers['U']
         self.p_scaler = scalers['p']

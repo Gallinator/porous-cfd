75c254cad510381699e0e298f9ce7d310834d1a9
Gallinator <lcgallinator4@gmail.com>
2025-08-04 19:52:56 +0200

    data_generator: use locationInMesh instead of locationsInMesh
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- data_generator.py
+++ data_generator.py
@@ -117,10 +117,9 @@ def get_location_outside():
 
 def write_locations_in_mesh(case_path: str, loc_in_mesh):
     snappy_dict = FoamFile(f'{case_path}/system/snappyHexMeshDict')
-    locations_in_mesh = snappy_dict['castellatedMeshControls']['locationsInMesh']
-    locations_in_mesh[0][0][0:2] = loc_out_mesh
-    locations_in_mesh[1][0][0:2] = loc_in_mesh
-    snappy_dict['castellatedMeshControls']['locationsInMesh'] = locations_in_mesh
+    locations_in_mesh = snappy_dict['castellatedMeshControls']['locationInMesh']
+    locations_in_mesh[0:2] = loc_in_mesh
+    snappy_dict['castellatedMeshControls']['locationInMesh'] = locations_in_mesh
 
 
 def set_par_dict_coeffs(dict_path: str, n_proc: int):

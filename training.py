a8b591fd5fcd4212199208099f4f8341c5da742a
Gallinator <lcgallinator4@gmail.com>
2025-08-06 21:47:52 +0200

    training: fix scalers indices
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- training.py
+++ training.py
@@ -37,9 +37,9 @@ if __name__ == '__main__':
     val_data = FoamDataset('data/val', n_internal, n_boundary, n_obs, 'data/train', rng=rng)
     val_loader = DataLoader(val_data, batch_size, False, num_workers=8, pin_memory=True)
 
-    scalers = {'U': train_data.standard_scaler[2:4],
-               'p': train_data.standard_scaler[4],
-               'Points': train_data.standard_scaler[0:2],
+    scalers = {'U': train_data.standard_scaler[3:6],
+               'p': train_data.standard_scaler[6],
+               'Points': train_data.standard_scaler[0:3],
                'd': train_data.d_normalizer,
                'f': train_data.f_normalizer}
 

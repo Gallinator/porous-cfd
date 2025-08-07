9de2529cfa20553de168f5b7b303d057cb125a66
Gallinator <lcgallinator4@gmail.com>
2025-08-07 21:19:03 +0200

    MomentumLoss: add Forcheimer equation to source term
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/losses.py
+++ models/losses.py
@@ -39,12 +39,14 @@ class MomentumLoss(nn.Module):
         norm_d_ui_j = (self.u_scaler.std[i] / self.points_scaler.std[j])
         norm_dd_ui_i = norm_d_ui_i * (1 / self.points_scaler.std[i])
         norm_dd_ui_j = norm_d_ui_j * (1 / self.points_scaler.std[j])
+        ui_raw = ui * self.u_scaler.std[i] + self.u_scaler.mean[i]
+        uj_raw = uj * self.u_scaler.std[j] + self.u_scaler.mean[j]
 
-        return (norm_d_ui_i * d_ui_i * (ui * self.u_scaler.std[i] + self.u_scaler.mean[i]) +
-                norm_d_ui_j * d_ui_j * (uj * self.u_scaler.std[j] + self.u_scaler.mean[j]) -
+        source = ui_raw * (self.d * self.mu * zones_ids + 1 / 2 * torch.sqrt(ui ** 2 + uj ** 2) * self.f)
+
+        return (norm_d_ui_i * d_ui_i * ui_raw + norm_d_ui_j * d_ui_j * uj_raw -
                 self.mu * (norm_dd_ui_i * dd_ui_i + norm_dd_ui_j * dd_ui_j) +
-                (self.p_stats.std / self.points_scaler.std[i]) * d_p_i +
-                (ui * self.u_scaler.std[i] + self.u_scaler.mean[i]) * self.d * self.mu * zones_ids)
+                (self.p_stats.std / self.points_scaler.std[i]) * d_p_i + source)
 
     def forward(self, *args):
         res = self.func(*args)

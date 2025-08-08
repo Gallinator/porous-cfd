cf66d58ce289514e76f06c134417dcf4b808b0ee
Gallinator <lcgallinator4@gmail.com>
2025-08-08 14:08:08 +0200

    MomentumLoss: pass f normalizer
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/pi_gano.py
+++ models/pi_gano.py
@@ -83,11 +83,12 @@ class PiGano(L.LightningModule):
         self.p_scaler = scalers['p']
         self.points_scaler = scalers['Points']
         self.d_scaler = scalers['d']
+        self.f_scaler = scalers['f']
 
         self.momentum_x_loss = MomentumLoss(0, 1, self.mu, self.u_scaler, self.points_scaler, self.p_scaler,
-                                            self.d_scaler)
+                                            self.d_scaler, self.f_scaler)
         self.momentum_y_loss = MomentumLoss(1, 0, self.mu, self.u_scaler, self.points_scaler, self.p_scaler,
-                                            self.d_scaler)
+                                            self.d_scaler, self.f_scaler)
         self.continuity_loss = ContinuityLoss(self.u_scaler, self.points_scaler)
 
         self.verbose_predict = False
--- training.py
+++ training.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
     scalers = {'U': train_data.standard_scaler[2:4],
                'p': train_data.standard_scaler[4],
                'Points': train_data.standard_scaler[0:2],
-               'd': train_data.d_normalizer}
+               'd': train_data.d_normalizer,
+               'f': train_data.f_normalizer}
 
     model = Pipn(train_data.domain_dict, scalers)
 

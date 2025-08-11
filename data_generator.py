45cde7671e9f1c7dc284a2d32b36b44fed7ee961
Gallinator <lcgallinator4@gmail.com>
2025-08-11 14:28:52 +0200

    data_generator: delete pre-split directory even if not empty
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- data_generator.py
+++ data_generator.py
@@ -290,7 +290,7 @@ def generate_split(data_path: str, config_path: str):
             shutil.move(f'{data_path}/{case}', f'{pathlib.Path(data_path).parent}/{s}/{case}')
         start = end
 
-    os.rmdir(data_path)
+    shutil.rmtree(data_path)
 
 
 def generate_min_points(data_parent: str):

0c66064164a936222eed49464a2ada0ab790ddcd
Gallinator <lcgallinator4@gmail.com>
2025-08-13 14:03:52 +0200

    inference: support saving plots to png, support custom data directories
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- inference.py
+++ inference.py
@@ -6,19 +6,19 @@ from pathlib import Path
 import numpy as np
 from lightning import Trainer
 from matplotlib import pyplot as plt
+from rich.progress import track
 from torch_geometric.loader import DataLoader
 
 from foam_dataset import FoamDataset, PdeData
 from models.pipn_pp import PipnPP, FoamData
 from visualization import plot_fields
 
-CHECKPOINT_PATH = 'lightning_logs/version_22/checkpoints/epoch=402-step=806.ckpt'
-
 
 def build_arg_parser() -> ArgumentParser:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--save-plots', action="store_true",
                             help='save all the inference plots', default=False)
+    last_model = sorted(os.listdir('lightning_logs'))[-1]
     default_model_path = Path('lightning_logs') / last_model / 'last.ckpt'
     arg_parser.add_argument('--checkpoint', type=str, default=default_model_path)
     arg_parser.add_argument('--data-dir', type=str, default='data/val')
@@ -26,32 +26,45 @@ def build_arg_parser() -> ArgumentParser:
     return arg_parser
 
 
-model = PipnPP.load_from_checkpoint(CHECKPOINT_PATH)
+if __name__ == '__main__':
+    args = build_arg_parser().parse_args()
 
-val_data = FoamDataset('data/val_unseen', 1000, 200, 500, 'data/train/raw')
-val_loader = DataLoader(val_data, 1, False, num_workers=8, pin_memory=True)
+    plots_path = None
+    if args.save_plots:
+        plots_path = Path('plots')
+        plots_path.mkdir(exist_ok=True)
+    model = PipnPP.load_from_checkpoint(args.checkpoint)
 
-trainer = Trainer(logger=False, enable_checkpointing=False)
-pred = trainer.predict(model, dataloaders=val_loader)[0]
-pred = PdeData(pred).numpy()
+    val_data = FoamDataset(args.data_dir, 1000, 200, 500, args.meta_dir)
+    val_loader = DataLoader(val_data, 1, False, num_workers=8, pin_memory=True)
 
-tgt = val_data[0]
+    trainer = Trainer(logger=False, enable_checkpointing=False)
+    predictions = trainer.predict(model, dataloaders=val_loader)
+    for i, (tgt, pred) in enumerate(track(list(zip(val_data, predictions)), description='Saving plots...')):
+        pred = PdeData(pred).numpy()
 
-points_scaler = val_data.standard_scaler[0:2]
-u_scaler = val_data.standard_scaler[2:4]
-p_scaler = val_data.standard_scaler[4]
+        case_plot_path = None
+        if plots_path is not None:
+            case_plot_path = plots_path / Path(args.data_dir).name / str(i)
+            case_plot_path.mkdir(exist_ok=True, parents=True)
 
-raw_points = points_scaler.inverse_transform(tgt.pos.numpy(force=True))
-ids = tgt.zones_ids.numpy(force=True)
+        points_scaler = val_data.standard_scaler[0:2]
+        u_scaler = val_data.standard_scaler[2:4]
+        p_scaler = val_data.standard_scaler[4]
 
-plt.interactive(True)
-plot_fields('Predicted', raw_points, u_scaler.inverse_transform(pred.u),
-            p_scaler.inverse_transform(pred.p), ids)
-plot_fields('Ground truth', raw_points, u_scaler.inverse_transform(tgt.pde.u.numpy(force=True)),
-            p_scaler.inverse_transform(tgt.pde.p.numpy(force=True)), ids)
+        raw_points = points_scaler.inverse_transform(tgt.pos.numpy(force=True))
+        ids = tgt.zones_ids.numpy(force=True)
 
-plt.interactive(False)
+        plt.interactive(case_plot_path is None)
 
-u_error = u_scaler.inverse_transform(pred.u) - u_scaler.inverse_transform(tgt.pde.u.numpy(force=True))
-p_error = p_scaler.inverse_transform(pred.p) - p_scaler.inverse_transform(tgt.pde.p.numpy(force=True))
-plot_fields('Absolute error', raw_points, np.abs(u_error), np.abs(p_error), ids, plot_streams=False)
+        plot_fields('Predicted', raw_points, u_scaler.inverse_transform(pred.u),
+                    p_scaler.inverse_transform(pred.p), ids, save_path=case_plot_path)
+        plot_fields('Ground truth', raw_points, u_scaler.inverse_transform(tgt.pde.u.numpy(force=True)),
+                    p_scaler.inverse_transform(tgt.pde.p.numpy(force=True)), ids, save_path=case_plot_path)
+
+        plt.interactive(False)
+
+        u_error = u_scaler.inverse_transform(pred.u) - u_scaler.inverse_transform(tgt.pde.u.numpy(force=True))
+        p_error = p_scaler.inverse_transform(pred.p) - p_scaler.inverse_transform(tgt.pde.p.numpy(force=True))
+        plot_fields('Absolute error', raw_points, np.abs(u_error), np.abs(p_error), ids,
+                    False, case_plot_path)

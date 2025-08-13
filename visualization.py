73f8b537ffa6461acd5742316a5d8ef9de08cdf5
Gallinator <lcgallinator4@gmail.com>
2025-08-13 12:46:27 +0200

    visualization: improve porous points markers
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- visualization.py
+++ visualization.py
@@ -26,11 +26,8 @@ def plot_or_save(fig, save_path):
 def plot_scalar_field(title: str, points: np.array, value: np.array, porous: np.array or None, fig, ax):
     ax.set_title(title, pad=20)
     porous_zone = np.nonzero(porous > 0)[0]
-    ax.scatter(points[porous_zone, 0], points[porous_zone, 1], points[porous_zone, 2], marker='$\circ$', s=100,
-               zorder=-1, c='silver',
-               label='Porous')
-    ax.scatter(points[porous_zone, 0], points[porous_zone, 1], points[porous_zone, 2], marker='$\circ$', s=50,
-               zorder=-1, c='black')
+    ax.scatter(points[porous_zone, 0], points[porous_zone, 1], points[porous_zone, 2], marker='o', s=50, zorder=-1, c=[0,0,0,0],
+               label='Porous',edgecolors='black')
     plot = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=value, s=0.5, cmap='turbo')
     fig.colorbar(plot, ax=ax)
     ax.legend(loc='upper right')

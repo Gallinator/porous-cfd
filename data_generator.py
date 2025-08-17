5efed49a993808c8dfc40381fb2dd77ccca8d97c
Gallinator <lcgallinator4@gmail.com>
2025-08-17 14:26:37 +0200

    data_generator: always remove original directory when splitting
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- data_generator.py
+++ data_generator.py
@@ -257,6 +257,8 @@ def generate_split(data_path: str, config_path: str):
         first_split = list(splits.keys())[0]
         shutil.move(f'{data_path}/{case}', f'{pathlib.Path(data_path).parent}/{first_split}/{case}')
 
+    shutil.rmtree(pathlib.Path(data_path))
+
 
 def build_arg_parser() -> ArgumentParser:
     arg_parser = argparse.ArgumentParser()

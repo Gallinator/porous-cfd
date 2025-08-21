ae26f9cbba6bc9fb5829cd6a9320b27e9d6587c9
Gallinator <lcgallinator4@gmail.com>
2025-08-21 13:21:52 +0200

    training: don't save checkpoints every epoch
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- training.py
+++ training.py
@@ -3,7 +3,7 @@ import os
 from argparse import ArgumentParser
 
 import torch
-from lightning.pytorch.callbacks import RichProgressBar, LearningRateMonitor
+from lightning.pytorch.callbacks import RichProgressBar, LearningRateMonitor, ModelCheckpoint
 from torch.utils.data import DataLoader
 from numpy.random import default_rng
 from foam_dataset import FoamDataset
@@ -53,9 +53,14 @@ if __name__ == '__main__':
 
     model = PiGano(train_data.domain_dict, scalers)
 
+    checkpoint_callback = ModelCheckpoint(filename='checkpoint-{epoch:d}', every_n_epochs=500, save_top_k=-1)
+
     trainer = L.Trainer(max_epochs=epochs,
+                        callbacks=[RichProgressBar(), checkpoint_callback, LearningRateMonitor()],
                         log_every_n_steps=int(len(train_data) / batch_size),
                         precision=args.precision,
                         default_root_dir=args.logs_dir)
 
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    trainer.save_checkpoint(f'{trainer.log_dir}/model.ckpt')

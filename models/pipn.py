87db313a28490a41d86e915991280a6392fb60da
Gallinator <lcgallinator4@gmail.com>
2025-08-25 19:15:03 +0200

    Pipn: update viscosity
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/pipn.py
+++ models/pipn.py
@@ -60,7 +60,7 @@ class Pipn(L.LightningModule):
         self.n_boundary = n_boundary
         self.encoder = Encoder()
         self.decoder = Decoder(3)
-        self.mu = 1489.4e-6  # As rho=1 mu and nu are the same
+        self.mu = 326.87e-6  # As rho=1 mu and nu are the same
         self.d = 14000
         self.f = 17.11
         self.training_loss_togger = LossLogger(self, 'Train loss',

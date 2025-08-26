bebf425d000e843475d443be135e5d97d668e1a6
Gallinator <lcgallinator4@gmail.com>
2025-08-26 12:21:25 +0200

    evaluation: plot porous and fluid region mae
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- evaluation.py
+++ evaluation.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
                 [avg_inference_time, val_timing['Average'] / 1e3],
                 plots_path)
 
-    errors, pred_residuals, cfd_residuals = [], [], []
+    errors, pred_residuals, cfd_residuals, zones_ids = [], [], [], []
     pde_scaler = val_data.standard_scaler[2:5].to_torch()
     for p, t in zip(pred, val_loader):
         pred_data, phys_data = p
@@ -70,17 +70,22 @@ if __name__ == '__main__':
         error = l1_loss(pde_scaler.inverse_transform(pred_data),
                         pde_scaler.inverse_transform(tgt_data.pde.data), reduction='none')
         errors.extend(error.numpy(force=True))
+        zones_ids.extend(tgt_data.zones_ids)
 
         pred_residuals.extend(phys_data.numpy(force=True))
         cfd_res = torch.cat([tgt_data.mom_x, tgt_data.mom_y, tgt_data.div], dim=-1)
         cfd_residuals.extend(cfd_res[..., :args.n_internal, :].numpy(force=True))
 
-    errors = np.concatenate(errors)
+    errors, zones_ids = np.concatenate(errors), np.array(zones_ids).flatten()
     error_data = PdeData(errors)
     plot_data_dist('Absolute error distribution', error_data.u, error_data.p, save_path=plots_path)
 
     mae = np.average(errors, axis=0)
-    plot_errors(mae.tolist(), save_path=plots_path)
+    plot_errors('MAE', mae.tolist(), save_path=plots_path)
+
+    porous_mae, fluid_mae = np.average(errors[zones_ids > 0,:], axis=0), np.average(errors[zones_ids < 1,:], axis=0)
+    plot_errors('Porous region MAE', porous_mae.tolist(), save_path=plots_path)
+    plot_errors('Fluid region MAE', fluid_mae.tolist(), save_path=plots_path)
 
     pred_residuals = np.concatenate(pred_residuals)
     cfd_residuals = np.concatenate(cfd_residuals)

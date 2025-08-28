5aea021ee1ff2aed3278c38c48c11e4aaa17721f
Gallinator <lcgallinator4@gmail.com>
2025-08-28 13:51:29 +0200

    visualization: use pyvista for 3d plots
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- visualization.py
+++ visualization.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from numpy.linalg import norm
+import vtk
+from pyvista import Plotter, PolyData
 from rich.progress import track
-from scipy.interpolate import griddata
-
 import data_parser
 from data_parser import parse_internal_mesh
 
@@ -33,35 +33,12 @@ def add_colorbar(fig, ax, plot):
     fig.colorbar(plot, cax=cax)
 
 
-def plot_scalar_field(title: str, points: np.array, value: np.array, porous: np.array or None, fig, ax):
-    ax.set_title(title, pad=20)
-    porous_zone = np.nonzero(porous > 0)[0]
-    ax.scatter(points[porous_zone, 0], points[porous_zone, 1], points[porous_zone, 2], marker='o', s=50, zorder=-1,
-               c='#ffffffff',
-               label='Porous', edgecolors='black')
-    plot = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=value, s=2, cmap='turbo')
-
-    ax.set_ymargin(0.025)
-    ax.set_xmargin(0.02)
-    fig.colorbar(plot, ax=ax)
-    ax.legend(loc='upper right')
-    ax.set_aspect('equal')
-
-
-def plot_uneven_stream(title: str, points: np.array, field: np.array, fig, ax):
-    ax.set_title(title, pad=20)
-    x = points[:, 0].flatten()
-    y = points[:, 1].flatten()
-    xx = np.linspace(x.min(), x.max(), 50)
-    yy = np.linspace(y.min(), y.max(), 50)
-
-    xi, yi = np.meshgrid(xx, yy)
-    field_x = field[:, 0].flatten()
-    field_y = field[:, 1].flatten()
-    field_s = norm(field, axis=1).flatten()
-    g_x = griddata(points, field_x, (xi, yi), method='nearest')
-    g_y = griddata(points, field_y, (xi, yi), method='nearest')
-    g_s = griddata(points, field_s, (xi, yi), method='nearest')
+def plot_scalar_field(title, points: np.array, value: np.array, zones_ids, plotter):
+    points = PolyData(points)
+    colorbar = {'title': title, 'vertical': True, 'position_y': 0.25, 'height': 0.5}
+    plotter.add_mesh(points, scalars=value, scalar_bar_args=colorbar, point_size=3.0)
+    plotter.show_grid(all_edges=True)
+    plotter.camera.position = (-80, -100, 50)
 
     plot = ax.streamplot(xx, yy, g_x, g_y, color=g_s, density=2, cmap='turbo')
     add_colorbar(fig, ax, plot.lines)
@@ -69,25 +46,21 @@ def plot_uneven_stream(title: str, points: np.array, field: np.array, fig, ax):
     ax.set_aspect('equal')
 
 
-def plot_fields(title: str, points: np.array, u: np.array, p: np.array, porous: np.array or None, plot_streams=True,
-                save_path=None):
-    fig = plt.figure(figsize=(16, 9), layout='constrained')
-    fig.suptitle(title, fontsize=20)
-    ax_u_x, ax_u_y, ax_u_z, ax_p = (fig.add_subplot(2, 2, 1, projection='3d'),
-                                    fig.add_subplot(2, 2, 2, projection='3d'),
-                                    fig.add_subplot(2, 2, 3, projection='3d'),
-                                    fig.add_subplot(2, 2, 4, projection='3d'))
+def plot_fields(title: str, points: np.array, u: np.array, p: np.array, porous: np.array or None, save_path=None):
+    plotter = Plotter(shape=(2, 2), off_screen=save_path is not None, window_size=[1920, 1080])
+
     # Pressure
-    plot_scalar_field(f'$p$ {M2_S2}', points, p, porous, fig, ax_p)
-
+    plotter.subplot(1, 1)
+    plot_scalar_field(rf'$p {M2_S2}$', points, p, porous, plotter)
     # Velocity
-    plot_scalar_field(f'$u_x$ {M_S}', points, u[:, 0], porous, fig, ax_u_x)
+    plotter.subplot(0, 0)
+    plot_scalar_field(rf'$u_x {M_S}$', points, u[:, 0], porous, plotter)
+    plotter.subplot(0, 1)
+    plot_scalar_field(rf'$u_y {M_S}$', points, u[:, 1], porous, plotter)
+    plotter.subplot(1, 0)
+    plot_scalar_field(rf'$u_z {M_S}$', points, u[:, 2], porous, plotter)
 
-    plot_scalar_field(f'$u_y$ {M_S}', points, u[:, 1], porous, fig, ax_u_y)
-
-    plot_scalar_field(f'$u_z$ {M_S}', points, u[:, 2], porous, fig, ax_u_z)
-
-    plot_or_save(fig, save_path)
+    plotter.show(screenshot=save_path if save_path else False)
 
 
 def plot_case(path: str):

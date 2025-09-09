7190b045622da835e2c3cbdc7569803a6b9f1641
Gallinator <lcgallinator4@gmail.com>
2025-09-09 13:40:38 +0200

    FoamData: move to own file No indices are used for features, indexing is based on labels and domain indices are used with gather
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

---
+++ dataset/foam_data.py
@@ -0,0 +1,40 @@
+import torch
+from torch import Tensor
+
+
+class FoamData:
+    def __init__(self, data: Tensor, labels: dict, domain: dict):
+        super().__init__()
+        self.data = data
+        self.labels = labels
+        self.domain = domain
+
+    def make_ids_gatherable(self, flat_ids):
+        return flat_ids.unsqueeze(dim=-1).repeat(1, 1, self.data.shape[-1])
+
+    def __getitem__(self, item):
+        if item in self.labels:
+            label = self.labels[item]
+            # Multi item label
+            if label:
+                sublabels = [self[l] for l in label]
+                return torch.cat(sublabels, dim=-1)
+            # Single item label
+            else:
+                col = list(self.labels.keys()).index(item)
+                return self.data[..., col:col + 1]
+
+        elif item in self.domain:
+            domain_ids = self.domain[item]
+            # Batched data
+            if len(self.data.shape) > 2:
+                gather_ids = self.make_ids_gatherable(domain_ids)
+                subdomain_data = torch.gather(self.data, 1, gather_ids)
+            else:
+                subdomain_data = self.data[domain_ids]
+            return FoamData(subdomain_data, self.labels,
+                            {item: torch.arange(0, len(domain_ids))})
+        else:
+            raise KeyError(f'{item} not found in labels or subdomains.'
+                           f' Available labels are {list(self.labels.keys())}. '
+                           f'Available subdomains are {list(self.domain.keys())}.')

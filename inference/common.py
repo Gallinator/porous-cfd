840ebf6fe2e226f029089e1e9abe8e1b10314597
Gallinator <lcgallinator4@gmail.com>
2025-09-11 22:04:13 +0200

    inference: add common functions
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

---
+++ inference/common.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Callable
+
+from lightning import Trainer
+from lightning.pytorch.callbacks import RichProgressBar
+from rich.progress import track
+from torch.utils.data import DataLoader
+
+from dataset.foam_data import FoamData
+from dataset.foam_dataset import collate_fn, FoamDataset
+
+
+def build_arg_parser() -> ArgumentParser:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--save-plots', action="store_true",
+                            help='save all the inference plots', default=False)
+    last_model = sorted(os.listdir('../lightning_logs'))[-1]
+    default_model_path = Path('../lightning_logs') / last_model / 'model.ckpt'
+    arg_parser.add_argument('--checkpoint', type=str, default=default_model_path)
+    arg_parser.add_argument('--data-dir', type=str, default='data/val')
+    arg_parser.add_argument('--meta-dir', type=str, default='data/train')
+    arg_parser.add_argument('--n-internal', type=int,
+                            help='number of internal points to sample', default=3000)
+    arg_parser.add_argument('--n-boundary', type=int,
+                            help='number of internal points to sample', default=700)
+    arg_parser.add_argument('--n-observations', type=int,
+                            help='number of observation points to sample', default=1200)
+    arg_parser.add_argument('--precision', type=str, default='bf16-mixed')
+    return arg_parser
+
+
+def create_plots_root(args):
+    plots_path = None
+    if args.save_plots:
+        plots_path = Path(args.checkpoint).parent / 'plots' / Path(args.data_dir).name
+        plots_path.mkdir(exist_ok=True, parents=True)
+    return plots_path
+
+
+def create_case_plot_dir(plots_root, case_name):
+    case_plot_dir = None
+    if plots_root is not None:
+        case_plot_dir = plots_root / case_name
+        case_plot_dir.mkdir(exist_ok=True, parents=True)
+    return case_plot_dir
+
+
+def predict(args, model, data: FoamDataset, result_process_fn: Callable[[FoamDataset, FoamData, FoamData, Path], None]):
+    data_loader = DataLoader(data, 1, False, num_workers=8, pin_memory=True, collate_fn=collate_fn)
+
+    trainer = Trainer(logger=False,
+                      enable_checkpointing=False,
+                      callbacks=[RichProgressBar()],
+                      precision=args.precision)
+    predictions = trainer.predict(model, dataloaders=data_loader)
+
+    for i, (tgt, pred) in enumerate(track(list(zip(data, predictions)), description='Saving plots...')):
+        pred = FoamData(pred[0], model.pred_labels, tgt.domain)
+        case_path = Path(data.samples[i])
+        result_process_fn(data, tgt, pred, case_path)

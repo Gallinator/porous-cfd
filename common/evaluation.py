063c43cf196b1597cae0783fb1a02074449c128c
Gallinator <lcgallinator4@gmail.com>
2025-09-13 15:33:04 +0200

    fix default log paths
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- common/evaluation.py
+++ common/evaluation.py
@@ -38,8 +38,8 @@ def build_arg_parser() -> ArgumentParser:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--save-plots', action="store_true",
                             help='save all the inference plots', default=False)
-    last_model = sorted(os.listdir('../lightning_logs'))[-1]
-    default_model_path = Path('../lightning_logs') / last_model / 'model.ckpt'
+    last_model = sorted(os.listdir('lightning_logs'))[-1]
+    default_model_path = Path('lightning_logs') / last_model / 'model.ckpt'
     arg_parser.add_argument('--checkpoint', type=str, default=default_model_path)
     arg_parser.add_argument('--data-dir', type=str, default='data/val')
     arg_parser.add_argument('--meta-dir', type=str, default='data/train')

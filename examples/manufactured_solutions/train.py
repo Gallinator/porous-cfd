3f8603bab6e6ef5ede0e7b671f72cbf940d3969e
Gallinator <lcgallinator4@gmail.com>
2025-09-14 11:26:58 +0200

    manufactured solutions: update parameters
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/manufactured_solutions/train.py
+++ examples/manufactured_solutions/train.py
@@ -12,9 +12,11 @@ if __name__ == '__main__':
     n_obs = args.n_observations
 
     rng = default_rng(8421)
-    train_data = ManufacturedDataset(args.train_dir, n_internal, n_boundary, 50, 1, rng=rng)
-    val_data = ManufacturedDataset(args.val_dir, n_internal, n_boundary, 50, 1, rng=rng, meta_dir=args.train_dir)
+    d = 50
+    f = 1
+    train_data = ManufacturedDataset(args.train_dir, n_internal, n_boundary, d, f, rng=rng)
+    val_data = ManufacturedDataset(args.val_dir, n_internal, n_boundary, d, f, rng=rng, meta_dir=args.train_dir)
 
-    model = PipnPorous()
+    model = PipnPorous(nu=0.01, d=d, f=f, in_dim=2, out_features=3)
 
     train(args, model, train_data, val_data)

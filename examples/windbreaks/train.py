7bd40ae910c6def8fb696ec114bf3475d6e8a927
Gallinator <lcgallinator4@gmail.com>
2025-09-15 19:06:14 +0200

    Windbreaks: use FixedLossScaler
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/windbreaks/train.py
+++ examples/windbreaks/train.py
@@ -2,6 +2,7 @@ from numpy.random import default_rng
 
 from common.training import train, build_arg_parser
 from dataset.foam_dataset import FoamDataset
+from models.losses import FixedLossScaler
 from models.pi_gano import PiGano
 
 if __name__ == '__main__':
@@ -14,6 +15,21 @@ if __name__ == '__main__':
     rng = default_rng(8421)
     train_data = FoamDataset(args.train_dir, n_internal, n_boundary, n_obs, rng=rng)
     val_data = FoamDataset(args.val_dir, n_internal, n_boundary, n_obs, rng=rng, meta_dir=args.train_dir)
-    model = PiGano(train_data.normalizers)
+    loss_scaler = FixedLossScaler({'continuity': [10],
+                                   'momentum': [10] * 3,
+                                   'boundary': [1] * 4,
+                                   'observations': [1] * 4})
+    model = PiGano(14.61e-6,
+                   3,
+                   4,
+                   10,
+                   [256, 256, 512],
+                   [256, 256, 256],
+                   [256, 256, 256],
+                   4,
+                   [0, 0.15, 0.15, 0, 0],
+                   train_data.normalizers,
+                   {'Subdomains': ['inlet', 'internal'], 'Features': ['Ux-inlet', 'd', 'f']},
+                   loss_scaler)
 
     train(args, model, train_data, val_data)

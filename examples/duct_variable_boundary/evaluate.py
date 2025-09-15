257b7333d8eb283c608b12bfed1ce4ea06c2d4f5
Gallinator <lcgallinator4@gmail.com>
2025-09-15 19:18:58 +0200

    Duct variable: use tensors in evaluation plots
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_variable_boundary/evaluate.py
+++ examples/duct_variable_boundary/evaluate.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 from numpy.random import default_rng
 from scipy.stats._mstats_basic import trimmed_mean
-from torch import Tensor
 from torch.nn.functional import l1_loss
 
 from common.evaluation import save_mae_to_csv, build_arg_parser, evaluate
@@ -13,9 +12,9 @@ from models.pi_gano import PiGano
 from visualization.common import plot_data_dist, plot_residuals, plot_errors
 
 
-def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: Tensor) -> tuple:
-    u_scaler = data.normalizers['U'].to(model.device)
-    p_scaler = data.normalizers['p'].to(model.device)
+def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: FoamData) -> tuple:
+    u_scaler = data.normalizers['U'].to()
+    p_scaler = data.normalizers['p'].to()
 
     # Domain
     u_error = l1_loss(u_scaler.inverse_transform(predicted['U']),
@@ -24,41 +23,44 @@ def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, ext
                       p_scaler.inverse_transform(target['p']), reduction='none')
     error = torch.cat([u_error, p_error], dim=-1)
 
-    zones_ids = target['cellToRegion'].numpy(force=True)
+    zones_ids = target['cellToRegion']
 
-    # Equation residuals
-    predicted_residuals = extras.numpy(force=True)
-    target_residuals = torch.cat([target['internal']['momentError'], target['internal']['div(phi)']], dim=-1)
-
-    return error, predicted_residuals, target_residuals, zones_ids
+    return (error,
+            extras['Momentum'],
+            extras['div'],
+            target['internal']['momentError'],
+            target['internal']['div(phi)'],
+            zones_ids)
 
 
 def postprocess_fn(data: FoamDataset, results: tuple, plots_path: Path):
-    errors, solid_errors, predicted_residuals, target_residuals, zones_ids = results
+    errors, predicted_momentum, predicted_div, target_momentum, target_div, zones_ids = results
 
-    errors, zones_ids = np.concatenate(errors)
-    zones_ids = np.array(zones_ids).flatten()
+    errors = torch.cat(errors)
+    zones_ids = torch.cat(zones_ids).flatten()
     plot_data_dist('Absolute error distribution',
                    errors[..., :model.dims],
                    errors[..., model.dims:],
                    save_path=plots_path)
-    mae = np.average(errors, axis=0).tolist()
+    mae = torch.mean(errors, dim=0).tolist()
     plot_errors('Average relative error', mae, save_path=plots_path)
 
-    fluid_mae = np.average(errors[zones_ids < 1, :], axis=0).tolist()
-    porous_mae = np.average(errors[zones_ids > 0, :], axis=0).tolist()
+    fluid_mae = torch.mean(errors[zones_ids < 1, :], dim=0).tolist()
+    porous_mae = torch.mean(errors[zones_ids > 0, :], dim=0).tolist()
     plot_errors('Porous region MAE', porous_mae, save_path=plots_path)
     plot_errors('Fluid region MAE', fluid_mae, save_path=plots_path)
 
-    pred_residuals = np.concatenate(predicted_residuals)
-    cfd_residuals = np.concatenate(target_residuals)
+    predicted_momentum, predicted_div = torch.cat(predicted_momentum), torch.cat(predicted_div)
+    target_momentum, target_div = torch.cat(target_momentum), torch.cat(target_div)
     plot_data_dist('Absolute residuals',
-                   np.abs(pred_residuals[..., :model.dims]),
-                   np.abs(pred_residuals[..., model.dims:]),
+                   np.abs(predicted_momentum),
+                   np.abs(predicted_div),
                    save_path=plots_path)
 
-    pred_res_avg = trimmed_mean(np.abs(pred_residuals), limits=[0, 0.05], axis=0)
-    cfd_res_avg = trimmed_mean(np.abs(cfd_residuals), limits=[0, 0.05], axis=0)
+    predicted_residuals = torch.cat([predicted_momentum, predicted_div], -1)
+    target_residuals = torch.cat([target_momentum, target_div], -1)
+    pred_res_avg = trimmed_mean(torch.abs(predicted_residuals), limits=[0, 0.05], axis=0)
+    cfd_res_avg = trimmed_mean(torch.abs(target_residuals), limits=[0, 0.05], axis=0)
     plot_residuals(pred_res_avg, cfd_res_avg, trim=0.05, save_path=plots_path)
 
     if args.save_plots:

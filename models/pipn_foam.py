56268ba29cf9beee9a7e4cf1331d2c4c34cf41b3
Gallinator <lcgallinator4@gmail.com>
2025-09-15 18:54:42 +0200

    PipnFoam: pass loss scaler
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- models/pipn_foam.py
+++ models/pipn_foam.py
@@ -9,8 +9,8 @@ from models.modules import PipnEncoder, PipnDecoder
 
 
 class PipnFoam(PorousPinnBase):
-    def __init__(self, nu, d, f, in_dim, out_features, scalers: dict[str, StandardScaler]):
-        super().__init__(out_features, nu)
+    def __init__(self, nu, d, f, in_dim, out_features, scalers: dict[str, StandardScaler], loss_scaler=None):
+        super().__init__(out_features, nu, True, loss_scaler)
         self.save_hyperparameters()
         self.encoder = PipnEncoder(in_dim, 64, 1024, [64], [96, 128])
         self.decoder = PipnDecoder(out_features, 64, 1024, [512, 256, 128],

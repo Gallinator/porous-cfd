a9eb3174e192dca73d5d9c8416671d0c052f2be5
Gallinator <lcgallinator4@gmail.com>
2025-09-22 13:16:53 +0200

    Fixed boundary: add get_model function
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_fixed_boundary/train.py
+++ examples/duct_fixed_boundary/train.py
@@ -3,7 +3,68 @@ from numpy.random import default_rng
 from common.training import build_arg_parser, train
 from dataset.foam_dataset import FoamDataset
 from models.losses import FixedLossScaler
-from models.pipn_foam import PipnFoam
+from models.pipn.pipn_foam import PipnFoam, PipnFoamPp, PipnFoamPpMrg, PipnFoamPpFull
+
+
+def get_model(name, normalizers):
+    loss_scaler = FixedLossScaler({'continuity': [1],
+                                   'momentum': [1] * 2,
+                                   'boundary': [1] * 3,
+                                   'observations': [100] * 3})
+    nu, d, f = 1489.4e-6, 14000, 17.11
+    match name:
+        case 'pipn':
+            return PipnFoam(nu=nu,
+                            d=d,
+                            f=f,
+                            fe_local_layers=[2, 64, 64],
+                            fe_global_layers=[64 + 1, 96, 128, 1024],
+                            seg_layers=[1024 + 64, 512, 256, 128, 3],
+                            seg_dropout=[0.0, 0.05, 0.05, 0],
+                            scalers=normalizers,
+                            loss_scaler=loss_scaler)
+        case 'pipn-pp':
+            return PipnFoamPp(nu=nu,
+                              d=d,
+                              f=f,
+                              fe_local_layers=[2, 64, 64],
+                              seg_layers=[1024 + 64, 512, 256, 128, 3],
+                              seg_dropout=[0.0, 0.05, 0.05, 0],
+                              fe_radius=[0.2, 0.5, 1],
+                              fe_fraction=[0.7, 0.5, 0.25],
+                              fe_global_layers=[[2 + 1 + 2, 64], [64 + 2, 128], [128 + 2, 128], [128 + 2, 1024]],
+                              scalers=normalizers,
+                              loss_scaler=loss_scaler)
+        case 'pipn-pp-mrg':
+            return PipnFoamPpMrg(nu=nu,
+                                 d=d,
+                                 f=f,
+                                 fe_local_layers=[2, 64, 64],
+                                 seg_layers=[1024 + 64, 512, 256, 128, 3],
+                                 seg_dropout=[0.0, 0.05, 0.05, 0],
+                                 scalers=normalizers,
+                                 loss_scaler=loss_scaler)
+        case 'pipn-pp-full':
+            return PipnFoamPpFull(nu=nu,
+                                  d=d,
+                                  f=f,
+                                  enc_layers=[[2 + 1 + 2, 64, 64, 128],
+                                              [128 + 2, 128, 128, 256],
+                                              [256 + 2, 1024]],
+                                  enc_radius=[0.2, 0.4],
+                                  enc_fraction=[0.5, 0.25],
+                                  dec_layers=[[1024 + 256, 256, 256],
+                                              [128 + 256, 128, 128],
+                                              [128 + 3, 128, 128, 128, 3]],
+                                  dec_k=[6, 6, 6],
+                                  last_dec_dropout=[0., 0., [0., 0.2, 0.2, 0.]],
+                                  scalers=normalizers,
+                                  loss_scaler=loss_scaler)
+        case _:
+            raise NotImplementedError
+
+    return
+
 
 if __name__ == '__main__':
     args = build_arg_parser().parse_args()
@@ -13,19 +74,9 @@ if __name__ == '__main__':
     n_obs = args.n_observations
 
     rng = default_rng(8421)
-    train_data = FoamDataset(args.train_dir, n_internal, n_boundary, n_obs, rng=rng)
+    train_data = FoamDataset(args.val_dir, n_internal, n_boundary, n_obs, rng=rng)
     val_data = FoamDataset(args.val_dir, n_internal, n_boundary, n_obs, rng=rng, meta_dir=args.train_dir)
 
-    loss_scaler = FixedLossScaler({'continuity': [1],
-                                   'momentum': [1] * 2,
-                                   'boundary': [1] * 3,
-                                   'observations': [100] * 3})
-    model = PipnFoam(nu=1489.4e-6,
-                     d=14000,
-                     f=17.11,
-                     in_dim=2,
-                     out_features=3,
-                     scalers=train_data.normalizers,
-                     loss_scaler=loss_scaler)
+    model = get_model(args.model, train_data.normalizers)
 
     train(args, model, train_data, val_data)

c5da39e3f6387183a67b2b3a81fc886b3d5cc77b
Gallinator <lcgallinator4@gmail.com>
2025-09-22 14:29:06 +0200

    Variable duct: add get_model to train script
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_variable_boundary/train.py
+++ examples/duct_variable_boundary/train.py
@@ -3,7 +3,44 @@ from numpy.random import default_rng
 from common.training import train, build_arg_parser
 from dataset.foam_dataset import FoamDataset
 from models.losses import FixedLossScaler
-from models.pi_gano import PiGano
+from models.pi_gano.pi_gano import PiGano
+from models.pi_gano.pi_gano_pp import PiGanoPp
+
+
+def get_model(name, normalizers):
+    loss_scaler = FixedLossScaler({'continuity': [1],
+                                   'momentum': [1] * 2,
+                                   'boundary': [1] * 3,
+                                   'observations': [100] * 3})
+    variable_boundaries = {'Subdomains': ['inlet', 'internal'], 'Features': ['U-inlet', 'd', 'f']}
+    match name:
+        case 'pi-gano':
+            return PiGano(1489.4e-6,
+                          3,
+                          [8, 128, 352, 352, 352],
+                          [3, 64, 176, 176, 176],
+                          [2, 64, 176, 176, 176],
+                          4,
+                          [0, 0.1, 0.1, 0, 0],
+                          normalizers,
+                          variable_boundaries,
+                          loss_scaler)
+        case 'pi-gano-pp':
+            return PiGanoPp(1489.4e-6,
+                            3,
+                            [8, 128, 352, 352, 352],
+                            [[2 + 1 + 2, 64], [64 + 2, 176], [176 + 2, 176], [176+ 2, 176]],
+                            [0.2, 0.5, 1],
+                            [0.7, 0.5, 0.25],
+                            [2, 64, 176, 176, 176],
+                            4,
+                            [0, 0.1, 0.1, 0, 0],
+                            normalizers,
+                            variable_boundaries,
+                            loss_scaler)
+        case _:
+            raise NotImplementedError
+
 
 if __name__ == '__main__':
     args = build_arg_parser().parse_args()
@@ -16,21 +53,6 @@ if __name__ == '__main__':
     train_data = FoamDataset(args.train_dir, n_internal, n_boundary, n_obs, rng=rng)
     val_data = FoamDataset(args.val_dir, n_internal, n_boundary, n_obs, rng=rng, meta_dir=args.train_dir)
 
-    loss_scaler = FixedLossScaler({'continuity': [1],
-                                   'momentum': [1] * 2,
-                                   'boundary': [1] * 3,
-                                   'observations': [100] * 3})
-    model = PiGano(1489.4e-6,
-                   2,
-                   3,
-                   8,
-                   [128, 352, 352, 352],
-                   [64, 176, 176, 176],
-                   [64, 176, 176, 176],
-                   4,
-                   [0, 0.1, 0.1, 0, 0],
-                   train_data.normalizers,
-                   {'Subdomains': ['inlet', 'internal'], 'Features': ['U-inlet', 'd', 'f']},
-                   loss_scaler)
+    model = get_model(args.model, train_data.normalizers)
 
     train(args, model, train_data, val_data)

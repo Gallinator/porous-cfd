0fbcfbc6cab6819ec4efebaeaad5151f092c5367
Gallinator <lcgallinator4@gmail.com>
2025-09-22 13:17:25 +0200

    Manufacture solutions: add get_model function
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/manufactured_solutions/train.py
+++ examples/manufactured_solutions/train.py
@@ -2,7 +2,25 @@ from numpy.random import default_rng
 
 from common.training import build_arg_parser, train
 from manufactured_dataset import ManufacturedDataset
-from models.pipn_baseline import PipnPorous
+from models.pipn.pipn_baseline import PipnManufactured, PipnManufacturedPorousPp
+
+
+def get_model(name):
+    if name == 'pipn':
+        return PipnManufactured(nu=0.01, d=d, f=f,
+                                fe_local_layers=[2, 64, 64],
+                                fe_global_layers=[64 + 1, 64, 128, 1024],
+                                seg_layers=[1024 + 64, 512, 256, 128, 3])
+    elif name == 'pipn-pp':
+        return PipnManufacturedPorousPp(nu=0.01, d=d, f=f,
+                                        fe_local_layers=[2, 64, 64],
+                                        fe_global_layers=[[2 + 1 + 2, 64], [64 + 2, 128], [128 + 2, 1024]],
+                                        fe_global_radius=[0.5, 0.25],
+                                        fe_global_fraction=[0.6, 1.2],
+                                        seg_layers=[1024 + 64, 512, 256, 128, 3])
+    else:
+        raise NotImplementedError()
+
 
 if __name__ == '__main__':
     args = build_arg_parser().parse_args()
@@ -17,6 +35,6 @@ if __name__ == '__main__':
     train_data = ManufacturedDataset(args.train_dir, n_internal, n_boundary, d, f, rng=rng)
     val_data = ManufacturedDataset(args.val_dir, n_internal, n_boundary, d, f, rng=rng, meta_dir=args.train_dir)
 
-    model = PipnPorous(nu=0.01, d=d, f=f, in_dim=2, out_features=3)
+    model = get_model(args.model)
 
     train(args, model, train_data, val_data)

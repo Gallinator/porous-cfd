9759346f1fb34e91cf72489d107703a25de67072
Gallinator <lcgallinator4@gmail.com>
2025-09-22 23:22:29 +0200

    Windbreaks: add PiGanoFull to train script
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/windbreaks/train.py
+++ examples/windbreaks/train.py
@@ -5,6 +5,7 @@ from dataset.foam_dataset import FoamDataset
 from models.losses import FixedLossScaler
 from models.pi_gano.pi_gano import PiGano
 from models.pi_gano.pi_gano_pp import PiGanoPp
+from models.pi_gano.pi_gano_pp_full import PiGanoPpFull
 
 
 def get_model(name, normalizers):
@@ -38,6 +39,19 @@ def get_model(name, normalizers):
                             normalizers,
                             variable_boundaries,
                             loss_scaler)
+        case 'pi-gano-pp-full':
+            return PiGanoPpFull(nu=14.61e-6,
+                                out_features=4,
+                                branch_layers=[10, 256, 256, 256],
+                                enc_layers=[[3 + 1 + 3, 64, 64, 128], [128 + 3, 128, 128, 256], [256 + 3, 512]],
+                                enc_radius=[0.2, 0.5, 1],
+                                enc_fraction=[0.7, 0.5, 0.25],
+                                dec_layers=[[512 + 256, 256, 256], [128 + 256, 128, 128], [128 + 4, 128, 128, 128, 4]],
+                                dec_k=[6, 6, 6],
+                                fp_dropout=[0., 0., [0., 0.2, 0.2, 0.]],
+                                scalers=normalizers,
+                                loss_scaler=loss_scaler,
+                                variable_boundaries=variable_boundaries)
         case _:
             raise NotImplementedError
 

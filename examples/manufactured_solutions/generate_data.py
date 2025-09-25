bcfd202f98ddc58dace30437caf4cd4c75bfd557
Gallinator <lcgallinator4@gmail.com>
2025-09-25 15:38:09 +0200

    Manufactured solutions: print working directory
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/manufactured_solutions/generate_data.py
+++ examples/manufactured_solutions/generate_data.py
@@ -5,6 +5,7 @@ from examples.manufactured_solutions.manufactured_generator import GeneratorManu
 
 if __name__ == '__main__':
     print(os.environ)
+    print(os.getcwd())
     args = build_arg_parser().parse_args()
     openfoam_cmd = f'{args.openfoam_dir}/etc/openfoam'
     generator = GeneratorManufactured('assets', openfoam_cmd, args.openfoam_procs, meta_only=args.meta_only)

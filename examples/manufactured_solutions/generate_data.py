8f2596b83d7a8d65b755bac142d3cb3171adee7b
Gallinator <lcgallinator4@gmail.com>
2025-09-25 16:14:50 +0200

    Revert "Manufactured solutions: print info"
    This reverts commit 18a5aa82cbcf27c3683bbbfd7a6788d1ab8afee4.

--- examples/manufactured_solutions/generate_data.py
+++ examples/manufactured_solutions/generate_data.py
@@ -1,12 +1,11 @@
 import os
 
-print(os.environ)
-print(os.getcwd())
-
 from datagen.data_generator import build_arg_parser
 from examples.manufactured_solutions.manufactured_generator import GeneratorManufactured
 
 if __name__ == '__main__':
+    print(os.environ)
+    print(os.getcwd())
     args = build_arg_parser().parse_args()
     openfoam_cmd = f'{args.openfoam_dir}/etc/openfoam'
     generator = GeneratorManufactured('assets', openfoam_cmd, args.openfoam_procs, meta_only=args.meta_only)

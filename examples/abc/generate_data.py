cedc8fb88eae18f7b25d0913bdadebb674ebe624
Gallinator <lcgallinator4@gmail.com>
2025-09-25 14:29:48 +0200

    Abc: enable data extraction
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/abc/generate_data.py
+++ examples/abc/generate_data.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from random import Random
-
 from datagen.data_generator import build_arg_parser
 from examples.abc.abc_generator import AbcGenerator
+from examples.abc.data_preprocess import move_to_meshes, extract
 
 if __name__ == '__main__':
     args = build_arg_parser().parse_args()
@@ -10,9 +10,7 @@ if __name__ == '__main__':
     data_root.mkdir(exist_ok=True, parents=True)
     rng = Random(8421)
     # download('https://archive.nyu.edu/retrieve/89085/abc_0000_obj_v00.7z', data_root / 'Abc.7z')
-    # extract(data_root / 'Abc.7z', data_root / 'Abc')
-
-    # move_to_meshes(data_root / 'Abc', 'assets/meshes/standard', 300, rng)
+    extract('assets/abc.tar.gz', 'assets/meshes/standard')
 
     openfoam_cmd = f'{args.openfoam_dir}/etc/openfoam'
     generator = AbcGenerator('assets', openfoam_cmd, args.openfoam_procs, 0.5, args.meta_only)

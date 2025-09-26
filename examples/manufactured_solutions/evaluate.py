4d0db3112b3f7f741644373b443db6c628a54639
Gallinator <lcgallinator4@gmail.com>
2025-09-26 22:05:20 +0200

    Manufactured solutions: remove common plots from evaluation
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/manufactured_solutions/evaluate.py
+++ examples/manufactured_solutions/evaluate.py
@@ -25,52 +25,11 @@ def get_model(checkpoint):
 
 
 def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: FoamData) -> tuple:
-    # Domain
-    u_error = l1_loss(predicted['U'], target['U'], reduction='none')
-    p_error = l1_loss(predicted['p'], target['p'], reduction='none')
-    error = torch.cat([u_error, p_error], dim=-1)
-
-    zones_ids = target['cellToRegion']
-
-    # Equation residuals
-    momentum_residuals = extras['Momentum']
-    div_residuals = extras['div']
-
-    return error, momentum_residuals, div_residuals, zones_ids
+    pass
 
 
 def postprocess_fn(data: FoamDataset, results: tuple, plots_path: Path):
-    errors, momentum_residuals, div_residuals, zones_ids = results
-
-    errors = np.concatenate(errors)
-    zones_ids = np.array(zones_ids).flatten()
-    plot_data_dist('Absolute error distribution',
-                   errors[..., :data.n_dims],
-                   errors[..., data.n_dims:],
-                   save_path=plots_path)
-    mae = np.average(errors, axis=0).tolist()
-    plot_errors('Average relative error', mae, save_path=plots_path)
-
-    fluid_mae = np.average(errors[zones_ids < 1, :], axis=0).tolist()
-    porous_mae = np.average(errors[zones_ids > 0, :], axis=0).tolist()
-    plot_errors('Porous region MAE', porous_mae, save_path=plots_path)
-    plot_errors('Fluid region MAE', fluid_mae, save_path=plots_path)
-
-    momentum_residuals = np.concatenate(momentum_residuals)
-    div_residuals = np.concatenate(div_residuals)
-    plot_data_dist('Absolute residuals',
-                   np.abs(momentum_residuals),
-                   np.abs(div_residuals),
-                   save_path=plots_path)
-
-    predicted_residuals = np.concatenate([momentum_residuals, div_residuals], -1)
-    predicted_res_avg = trimmed_mean(np.abs(predicted_residuals), limits=[0, 0.05], axis=0)
-    cfd_residuals_avg = np.zeros_like(predicted_res_avg)
-    plot_residuals(predicted_res_avg, cfd_residuals_avg, trim=0.05, save_path=plots_path)
-
-    if args.save_plots:
-        errors_dict = {'Total': mae, 'Fluid': fluid_mae, 'Porous': porous_mae}
-        save_mae_to_csv(errors_dict, ['Ux', 'Uy'][:model.dims] + ['p'], plots_path)
+    pass
 
 
 if __name__ == '__main__':

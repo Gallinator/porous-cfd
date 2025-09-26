34af799a346952b0f3cae203a4efb5baef5a76bf
Gallinator <lcgallinator4@gmail.com>
2025-09-26 22:04:05 +0200

    Duct fixed hard: remove common plots from evaluation
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_fixed_boundary_hard/evaluate.py
+++ examples/duct_fixed_boundary_hard/evaluate.py
@@ -1,18 +1,13 @@
 from pathlib import Path
-import numpy as np
+from typing import Any
 import torch
 from numpy.random import default_rng
-from scipy.stats._mstats_basic import trimmed_mean
-from torch.nn.functional import l1_loss
-
-from common.evaluation import save_mae_to_csv, build_arg_parser, evaluate, get_normalized_signed_distance, \
-    get_mean_max_error_distance
+from common.evaluation import build_arg_parser, evaluate, get_normalized_signed_distance, get_mean_max_error_distance
 from dataset.data_parser import parse_model_type
 from dataset.foam_data import FoamData
 from dataset.foam_dataset import FoamDataset
 from models.pipn.pipn_foam import PipnFoam, PipnFoamPp, PipnFoamPpMrg, PipnFoamPpFull
-from visualization.common import plot_data_dist, plot_residuals, plot_errors
-from visualization.common import box_plot
+from visualization.common import plot_errors
 
 
 def get_model(checkpoint):
@@ -30,71 +25,19 @@ def get_model(checkpoint):
             raise NotImplementedError
 
 
-def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: FoamData) -> tuple:
-    u_scaler = data.normalizers['U'].to(model.device)
-    p_scaler = data.normalizers['p'].to(model.device)
+def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: FoamData) -> dict[str, Any]:
+    c_scaler = data.normalizers['C'].to()
+    all_points = c_scaler.inverse_transform(target['C'])
+    interface_points = c_scaler.inverse_transform(target['interface']['C'])
 
-    # Domain
-    u_error = l1_loss(u_scaler.inverse_transform(predicted['U']),
-                      u_scaler.inverse_transform(target['U']), reduction='none')
-    p_error = l1_loss(p_scaler.inverse_transform(predicted['p']),
-                      p_scaler.inverse_transform(target['p']), reduction='none')
-    error = torch.cat([u_error, p_error], dim=-1)
-
-    zones_ids = target['cellToRegion']
-
-    interface_dist = get_normalized_signed_distance(target['C'], target['interface']['C'])
-
-    # Equation residuals
-    target_residuals = torch.cat([target['internal']['momentError'], target['internal']['div(phi)']], dim=-1)
-
-    return error, extras['Momentum'], extras['div'], target_residuals, zones_ids, interface_dist
+    interface_dist = get_normalized_signed_distance(all_points, interface_points)
+    return {'Interface distance': interface_dist}
 
 
-def postprocess_fn(data: FoamDataset, results: tuple, plots_path: Path):
-    errors, predicted_momentum, predicted_div, target_residuals, zones_ids, interface_dist = results
-
-    interface_dist = np.concatenate(interface_dist)
-    errors = np.array(errors)
-    interface_highest_distance = get_mean_max_error_distance(errors, 0.8, interface_dist)
-    plot_errors('Errors mean normalized distance from interface', interface_highest_distance, save_path=plots_path)
-
-    max_error_per_case = np.max(errors, axis=1)
-    box_plot('Maximum errors per case',
-             [*np.hsplit(max_error_per_case, errors.shape[-1])],
-             [f'$U_x$', f'$U_y$', f'$p$'],
-             plots_path)
-
-    errors = np.concatenate(errors)
-    zones_ids = np.array(zones_ids).flatten()
-    plot_data_dist('Absolute error distribution',
-                   errors[..., :data.n_dims],
-                   errors[..., data.n_dims:],
-                   save_path=plots_path)
-    mae = np.average(errors, axis=0).tolist()
-    plot_errors('Average relative error', mae, save_path=plots_path)
-
-    fluid_mae = np.average(errors[zones_ids < 1, :], axis=0).tolist()
-    porous_mae = np.average(errors[zones_ids > 0, :], axis=0).tolist()
-    plot_errors('Porous region MAE', porous_mae, save_path=plots_path)
-    plot_errors('Fluid region MAE', fluid_mae, save_path=plots_path)
-
-    predicted_div = np.concatenate(predicted_div)
-    predicted_momentum = np.concatenate(predicted_momentum)
-    plot_data_dist('Absolute residuals',
-                   np.abs(predicted_momentum),
-                   np.abs(predicted_div),
-                   save_path=plots_path)
-
-    target_residuals = np.concatenate(target_residuals)
-    predicted_residuals = np.concatenate([predicted_momentum, predicted_div], axis=-1)
-    pred_res_avg = trimmed_mean(np.abs(predicted_residuals), limits=[0, 0.05], axis=0)
-    cfd_res_avg = trimmed_mean(np.abs(target_residuals), limits=[0, 0.05], axis=0)
-    plot_residuals(pred_res_avg, cfd_res_avg, trim=0.05, save_path=plots_path)
-
-    if args.save_plots:
-        errors_dict = {'Total': mae, 'Fluid': fluid_mae, 'Porous': porous_mae}
-        save_mae_to_csv(errors_dict, ['Ux', 'Uy'] + ['p'], plots_path)
+def postprocess_fn(data: FoamDataset, results: dict[str, Any], plots_path: Path):
+    errors = torch.cat([results['U error'], results['p error']], -1)
+    max_error_from_interface = get_mean_max_error_distance(errors, 0.8, results['Interface distance'])
+    plot_errors('Errors mean normalized distance from interface', max_error_from_interface, save_path=plots_path)
 
 
 if __name__ == '__main__':

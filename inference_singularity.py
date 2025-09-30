fa5945ff81f2622a442ed783397b3f6407135a93
Gallinator <lcgallinator4@gmail.com>
2025-09-30 21:28:49 +0200

    inference_singularity: initial commit
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

---
+++ inference_singularity.py
@@ -0,0 +1,8 @@
+import importlib
+import os
+
+if __name__ == '__main__':
+    example_dir = os.environ['EXAMPLE']
+    example_dir = example_dir.replace('/', '.')
+    train_script = importlib.import_module(f'{example_dir}.inference')
+    train_script.inference()

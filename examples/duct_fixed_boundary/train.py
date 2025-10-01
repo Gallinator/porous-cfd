4045d32329f10048d295dc3d79e0eb6635d04bda
Gallinator <lcgallinator4@gmail.com>
2025-10-01 14:55:56 +0200

    Duct fixed boundary: update models parameters
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_fixed_boundary/train.py
+++ examples/duct_fixed_boundary/train.py
@@ -1,4 +1,5 @@
 from numpy.random import default_rng
+from torch.nn import Tanh
 
 from common.training import build_arg_parser, train
 from dataset.foam_dataset import FoamDataset
@@ -12,51 +13,58 @@ def get_model(name, normalizers):
                                    'boundary': [1] * 3,
                                    'observations': [100] * 3})
     nu, d, f = 1489.4e-6, 14000, 17.11
+    n_dim = 2
+    n_boundary_ids = 4
     match name:
         case 'pipn':
             return PipnFoam(nu=nu,
                             d=d,
                             f=f,
-                            fe_local_layers=[2, 64, 64],
-                            fe_global_layers=[64 + 1, 96, 128, 1024],
+                            fe_local_layers=[n_dim, 64, 64],
+                            fe_global_layers=[64 + 1 + n_boundary_ids, 96, 128, 1024],
                             seg_layers=[1024 + 64, 512, 256, 128, 3],
                             seg_dropout=[0.05, 0.05, 0, 0],
                             scalers=normalizers,
-                            loss_scaler=loss_scaler)
+                            loss_scaler=loss_scaler,
+                            activation=Tanh)
         case 'pipn-pp':
             return PipnFoamPp(nu=nu,
                               d=d,
                               f=f,
-                              fe_local_layers=[2, 64, 64],
-                              seg_layers=[1024 + 64, 512, 256, 128, 3],
-                              seg_dropout=[0.05, 0.05, 0, 0],
-                              fe_radius=[0.2, 0.5, 1],
-                              fe_fraction=[0.7, 0.5, 0.25],
-                              fe_global_layers=[[2 + 1 + 2, 64], [64 + 2, 128], [128 + 2, 128], [128 + 2, 1024]],
+                              fe_local_layers=[n_dim, 64, 64],
+                              seg_layers=[1024 + 64, 256, 128, 3],
+                              seg_dropout=[0.025, 0.01, 0],
+                              fe_radius=[0.5, 1],
+                              fe_fraction=[0.5, 0.25],
+                              fe_global_layers=[[n_dim + n_boundary_ids + 2, 64, 64],
+                                                [64 + n_dim, 128, 128],
+                                                [128 + n_dim, 256, 1024]],
                               scalers=normalizers,
                               loss_scaler=loss_scaler)
         case 'pipn-pp-mrg':
             return PipnFoamPpMrg(nu=nu,
                                  d=d,
                                  f=f,
-                                 fe_local_layers=[2, 64, 64],
+                                 fe_local_layers=[n_dim, 64, 64],
                                  seg_layers=[1024 + 64, 384, 128, 3],
-                                 seg_dropout=[0.05, 0, 0],
+                                 seg_dropout=[0.1, 0, 0],
                                  scalers=normalizers,
-                                 loss_scaler=loss_scaler)
+                                 loss_scaler=loss_scaler,
+                                 n_dims=n_dim,
+                                 mrg_in_features=n_boundary_ids)
         case 'pipn-pp-full':
             return PipnFoamPpFull(nu=nu,
                                   d=d,
                                   f=f,
-                                  enc_layers=[[2 + 1 + 2, 64, 64, 128],
-                                              [128 + 2, 128, 128, 256],
-                                              [256 + 2, 1024]],
+                                  enc_layers=[[n_dim * 2 + 1 + n_boundary_ids, 64, 64, 128],
+                                              [128 + n_dim, 128, 128, 256],
+                                              [256 + n_dim, 1024]],
                                   enc_radius=[0.2, 0.4],
                                   enc_fraction=[0.5, 0.25],
                                   dec_layers=[[1024 + 256, 256, 256],
                                               [128 + 256, 128, 128],
-                                              [128 + 3, 128, 128, 128, 3]],
-                                  dec_k=[6, 6, 6],
+                                              [128 + n_boundary_ids + n_dim + 1, 128, 128, 128, 3]],
+                                  dec_k=[3, 3, 3],
                                   last_dec_dropout=[0., 0., [0., 0.2, 0.2, 0.]],
                                   scalers=normalizers,
                                   loss_scaler=loss_scaler)

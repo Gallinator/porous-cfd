f365610e649f9e2e56f30b0797e69799939f8f9a
Gallinator <lcgallinator4@gmail.com>
2025-10-01 14:50:34 +0200

    Abc: update models parameters
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/abc/train.py
+++ examples/abc/train.py
@@ -13,13 +13,14 @@ def get_model(name, normalizers):
                                    'boundary': [1] * (n_dims + 1),
                                    'observations': [100] * (n_dims + 1)})
     nu, d, f = 1489.4e-6, 140000, 299.5
+    n_boundary_ids = 4
     match name:
         case 'pipn':
             return PipnFoam(nu=nu,
                             d=d,
                             f=f,
                             fe_local_layers=[n_dims, 64, 64],
-                            fe_global_layers=[64 + 1, 96, 128, 1024],
+                            fe_global_layers=[64 + n_boundary_ids + 1, 96, 128, 1024],
                             seg_layers=[1024 + 64, 512, 256, 128, n_dims + 1],
                             seg_dropout=[0.05, 0.05, 0, 0],
                             scalers=normalizers,
@@ -33,7 +34,7 @@ def get_model(name, normalizers):
                               seg_dropout=[0.05, 0.05, 0, 0],
                               fe_radius=[0.2, 0.5, 1],
                               fe_fraction=[0.7, 0.5, 0.25],
-                              fe_global_layers=[[n_dims + 1 + n_dims, 64],
+                              fe_global_layers=[[n_dims + n_boundary_ids + n_dims, 64],
                                                 [64 + n_dims, 128],
                                                 [128 + n_dims, 128],
                                                 [128 + n_dims, 1024]],
@@ -47,20 +48,22 @@ def get_model(name, normalizers):
                                  seg_layers=[1024 + 64, 384, 128, n_dims + 1],
                                  seg_dropout=[0.05, 0, 0],
                                  scalers=normalizers,
-                                 loss_scaler=loss_scaler)
+                                 loss_scaler=loss_scaler,
+                                 n_dims=n_dims,
+                                 mrg_in_features=n_boundary_ids + n_dims)
         case 'pipn-pp-full':
             return PipnFoamPpFull(nu=nu,
                                   d=d,
                                   f=f,
-                                  enc_layers=[[n_dims + 1 + n_dims, 64, 64, 128],
+                                  enc_layers=[[n_dims + n_boundary_ids + 1 + n_dims, 64, 64, 128],
                                               [128 + n_dims, 128, 128, 256],
                                               [256 + n_dims, 1024]],
                                   enc_radius=[0.4, 0.8],
                                   enc_fraction=[0.5, 0.25],
                                   dec_layers=[[1024 + 256, 256, 256],
                                               [128 + 256, 128, 128],
-                                              [128 + n_dims + 1, 128, 128, 128, n_dims + 1]],
-                                  dec_k=[4, 4, 4],
+                                              [128 + n_dims + n_boundary_ids + 1, 128, 128, 128, n_dims + 1]],
+                                  dec_k=[3, 3, 3],
                                   last_dec_dropout=[0., 0., [0., 0.2, 0.2, 0.]],
                                   scalers=normalizers,
                                   loss_scaler=loss_scaler)

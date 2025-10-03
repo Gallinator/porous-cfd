2acc3609b61662d74679ac35a1eb623b4e834734
Gallinator <lcgallinator4@gmail.com>
2025-10-03 12:06:22 +0200

    Abc: download data automatically from drive
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/abc/generate_data.py
+++ examples/abc/generate_data.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from datagen.data_generator import build_arg_parser
 from examples.abc.abc_generator import AbcGenerator
-from examples.abc.data_preprocess import extract
+from examples.abc.data_preprocess import extract, download_from_gdrive
 
 
 def run():
     args = build_arg_parser().parse_args()
     data_root = Path(args.data_root_dir)
     data_root.mkdir(exist_ok=True, parents=True)
-    # download('https://archive.nyu.edu/retrieve/89085/abc_0000_obj_v00.7z', data_root / 'Abc.7z')
-    extract('assets/abc.tar.gz', 'assets/meshes/standard')
+    download_from_gdrive('1pOGB9vO_Jf3YeRemJSs0yC5nBBKZcRTH&confirm', 'assets/Abc.tar.gz')
+    extract('assets/Abc.tar.gz', 'assets/meshes/standard')
 
     openfoam_cmd = f'{args.openfoam_dir}/etc/openfoam'
     generator = AbcGenerator('assets', openfoam_cmd, args.openfoam_procs, 0.5, args.meta_only)

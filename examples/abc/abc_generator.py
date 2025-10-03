f06595fb89cd96d97f423a0cb146ea6e0d53f100
Gallinator <lcgallinator4@gmail.com>
2025-10-03 14:48:11 +0200

    Abc: copy walls mesh in AbcGenerator
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/abc/abc_generator.py
+++ examples/abc/abc_generator.py
@@ -78,6 +78,7 @@ class AbcGenerator(Generator3DBase):
             # Delete original
             ops.object.select_all(action='SELECT')
             ops.object.delete()
+            shutil.copyfile(f'{meshes_dir}/walls/walls.obj',f'{meshes_subfolder}/walls.obj')
 
     def generate_openfoam_cases(self, meshes_dir, dest_dir, case_config_dir, rng):
         dest_dir.mkdir(parents=True, exist_ok=True)
@@ -87,5 +88,6 @@ class AbcGenerator(Generator3DBase):
             shutil.copytree(self.case_template_dir, case_path)
 
             shutil.copyfile(f"{mesh_set}mesh.obj", f"{case_path}/constant/triSurface/mesh.obj")
+            shutil.copyfile(f"{mesh_set}walls.obj", f"{case_path}/constant/triSurface/walls.obj")
 
             self.set_decompose_par(f'{case_path}')

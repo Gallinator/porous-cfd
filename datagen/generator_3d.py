e167a6f61d41dd520bc0609fb9c49254dbf8262c
Gallinator <lcgallinator4@gmail.com>
2025-10-10 15:37:12 +0200

    Generator3DBase: new inside location algorithm
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- datagen/generator_3d.py
+++ datagen/generator_3d.py
@@ -2,13 +2,42 @@ import glob
 import subprocess
 from abc import abstractmethod
 from pathlib import Path
+import mathutils
+import numpy as np
 from rich.progress import track
-
+import bpy
+from bpy import ops
 from datagen.data_generator import DataGeneratorBase
 from datagen.momentum_error import write_momentum_error
 
 
 class Generator3DBase(DataGeneratorBase):
+    def get_location_inside(self, mesh: str):
+        ops.object.select_all(action='SELECT')
+        ops.object.delete()
+        self.import_mesh(mesh)
+        ops.object.select_all(action='SELECT')
+        obj = bpy.context.object
+
+        s_x, s_y, s_z = obj.dimensions / 2
+        x, y, z = np.meshgrid(np.linspace(-s_x, s_x, 20),
+                              np.linspace(-s_y, s_y, 20),
+                              np.linspace(-s_z, s_z, 20))
+        grid = np.stack([x.flatten(), y.flatten(), z.flatten()]).T
+
+        _, closest, normal, _ = zip(*[obj.closest_point_on_mesh(g) for g in grid])
+        dir = np.array(closest) - grid
+        dot = np.sum(np.array(normal) * dir, axis=-1)
+
+        inside_mask = dot.flatten() > 0
+        inside_grid = grid[inside_mask]
+        dist = np.linalg.norm(dir[inside_mask], axis=-1)
+        center = inside_grid[np.argmax(dist)]
+        center = obj.matrix_world @ mathutils.Vector(center)
+        ops.object.delete()
+
+        return np.array(center)
+
     def create_case_template_dirs(self):
         (self.case_template_dir / 'constant/triSurface').mkdir(parents=True, exist_ok=True)
 

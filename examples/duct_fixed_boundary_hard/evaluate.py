1704187c9e903d012f4e97ad68340369cddd9362
Gallinator <lcgallinator4@gmail.com>
2025-10-12 11:12:04 +0200

    Duct fixed hard: plot mean pressure drop in evaluation
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- examples/duct_fixed_boundary_hard/evaluate.py
+++ examples/duct_fixed_boundary_hard/evaluate.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from typing import Any
 import numpy as np
+import torch
 from numpy.random import default_rng
-from common.evaluation import build_arg_parser, evaluate, get_normalized_signed_distance, get_mean_max_error_distance
+from common.evaluation import build_arg_parser, evaluate, get_pressure_drop
 from dataset.data_parser import parse_model_type
 from dataset.foam_data import FoamData
 from dataset.foam_dataset import FoamDataset
 from models.pipn.pipn_foam import PipnFoam, PipnFoamPp, PipnFoamPpMrg, PipnFoamPpFull
-from visualization.common import plot_errors
+from visualization.common import plot_multi_bar
 
 
 def get_model(checkpoint):
@@ -26,18 +27,21 @@ def get_model(checkpoint):
 
 
 def sample_process(data: FoamDataset, predicted: FoamData, target: FoamData, extras: FoamData) -> dict[str, Any]:
-    c_scaler = data.normalizers['C'].to()
-    all_points = c_scaler.inverse_transform(target['C'])
-    interface_points = c_scaler.inverse_transform(target['interface']['C'])
+    p_scaler = data.normalizers['p'].to()
+    tgt_drop = get_pressure_drop(p_scaler.inverse_transform(target['inlet']['p']),
+                                 p_scaler.inverse_transform(target['outlet']['p']))
+    pred_drop = get_pressure_drop(p_scaler.inverse_transform(predicted['inlet']['p']),
+                                  p_scaler.inverse_transform(predicted['outlet']['p']))
 
-    interface_dist = get_normalized_signed_distance(all_points, interface_points)
-    return {'Interface distance': interface_dist}
+    return {'Predicted drop': pred_drop.item(),
+            'Target drop': tgt_drop.item()}
 
 
 def postprocess_fn(data: FoamDataset, results: dict[str, Any], plots_path: Path):
-    errors = np.concatenate([results['U error'], results['p error']], -1)
-    max_error_from_interface = get_mean_max_error_distance(errors, 0.8, results['Interface distance'])
-    plot_errors('Errors mean normalized distance from interface', max_error_from_interface, save_path=plots_path)
+    mean_tgt_drop = np.mean(results['Predicted drop'])
+    mean_pred_drop = np.mean(results['Target drop'])
+    plot_multi_bar('Pressure drop', {'Predicted': [mean_pred_drop], 'True': [mean_tgt_drop]},
+                   ['$p$'], plots_path)
 
 
 def run():

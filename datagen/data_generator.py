16fc5292586998b5aeb80b6247f96ad3f75a1fdc
Gallinator <lcgallinator4@gmail.com>
2025-10-19 17:22:56 +0200

    DataGeneratorBase: quick fox for ABC outliers
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- datagen/data_generator.py
+++ datagen/data_generator.py
@@ -325,6 +325,15 @@ class DataGeneratorBase:
         matplotlib.use('Agg')
 
         for split in glob.glob(f'{dest_dir}/*/'):
+
+            # Dirty fix for ABC outliers
+            for c in os.listdir(split):
+                if c in {'00006587_b3d0eb0739bc4f9ebfdff16c_trimesh_005',
+                         '00002721_98d8f78b1bb5495f9a193852_trimesh_007',
+                         '00005570_f223bd7b687042f7b569e766_trimesh_005'}:
+                    print(f'Deleted {c}')
+                    shutil.rmtree(f'{split}/{c}')
+
             split_path = Path(split)
             if split_path.name == 'plots':
                 continue

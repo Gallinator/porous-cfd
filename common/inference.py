7684b716998f7d09798830ae48a1728c51622109
Gallinator <lcgallinator4@gmail.com>
2025-10-27 23:49:01 +0100

    Revert "inference: set default precision to 32"
    This reverts commit daafd5c6
    
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- common/inference.py
+++ common/inference.py
@@ -33,7 +33,7 @@ def build_arg_parser() -> ArgumentParser:
                             help='number of boundary points to sample', default=200)
     arg_parser.add_argument('--n-observations', type=int,
                             help='number of observation points to sample', default=500)
-    arg_parser.add_argument('--precision', type=str, default='32',
+    arg_parser.add_argument('--precision', type=str, default='bf16-mixed',
                             help='model weight precision. Supports mixed precision')
     return arg_parser
 

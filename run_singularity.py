2a529138d084ac59ab099c4b6dfdc5f5a7c57d14
Gallinator <lcgallinator4@gmail.com>
2025-11-13 23:22:07 +0100

    Comment run_singularity
    Signed-off-by: Gallinator <lcgallinator4@gmail.com>

--- run_singularity.py
+++ run_singularity.py
@@ -1,6 +1,11 @@
 import importlib
 import os
 
+# This script allows to run experiments on Singularity containers. This is a hacky way of avoiding the setting of python environment variables which do not work on some clusters.
+# The script works by using the EXAMPLE environment variable, which gives the experiment directory, and the RUNCMD variable which gives the python script to run together with its arguments.
+# Then the example package is loaded and the working directory is set.
+# The environment variables are set by running the sbatch.sh script.
+
 if __name__ == '__main__':
     example_dir = os.environ['EXAMPLE']
     cmd = os.environ['RUNCMD']
